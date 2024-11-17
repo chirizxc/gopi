ee9c13cb5782d52dc9bfd8182b4216adb3d25ec6
chiri <2alivemafia@gmail.com>
2024-11-17 22:36:45 +0300

    add basic auth

--- cmd/main.go
+++ cmd/main.go
@@ -11,6 +11,7 @@ import (
 	"gopi/internal/server/handlers/get/gif"
 	"gopi/internal/server/handlers/get/gifs"
 	"gopi/internal/server/handlers/save"
+	auth "gopi/internal/server/middleware/auth"
 	l "gopi/internal/server/middleware/logger"
 	"gopi/internal/storage"
 	"os"
@@ -33,12 +34,13 @@ func main() {
 	defer db.Close()
 
 	s := &storage.Storage{Db: db}
+	authMiddleware := auth.New(cfg.HTTPServer.User, cfg.HTTPServer.Pass)
 
 	router := gin.New()
 	router.Use(l.New(log))
 
 	router.POST("/save", save.New(s))
-	router.DELETE("/delete/:id", del.New(s))
+	router.DELETE("/delete/:id", authMiddleware, del.New(s))
 	router.GET("/gif/:id", gif.New(s))
 	router.GET("/gifs", gifs.New(s))
 
